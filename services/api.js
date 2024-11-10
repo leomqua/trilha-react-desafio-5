99157233a071eb35adf7d051e8a26a76759d42b6
Leonardo Martins <leonardomqua@hotmail.com>
2024-11-10 00:48:41 -0300

    Desafio concluido

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
+    baseURL: 'https://dgasgdpddyjznrpwuqjw.supabase.co/rest/v1',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRnYXNnZHBkZHlqem5ycHd1cWp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzExMDI5NTgsImV4cCI6MjA0NjY3ODk1OH0.jFpPZH3jq6dOQY-B7AS82LJHktd5UBApWWDM7p2WFuI",
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRnYXNnZHBkZHlqem5ycHd1cWp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzExMDI5NTgsImV4cCI6MjA0NjY3ODk1OH0.jFpPZH3jq6dOQY-B7AS82LJHktd5UBApWWDM7p2WFuI"
     }
 })
--- utils/mdx-utils.js
+++ utils/mdx-utils.js
@@ -12,8 +12,15 @@ export const getPosts = async () => {
 
 export const getPostBySlug = async (id) => {
 
-    //TODO: BUSCAR UM POST EM ESPECIFICO.
-    //const {data} = await api.get(`/post?id=eq.${id}`)
+    try {
+        const { data } = await api.get(`/posts?id=eq.${id}`);
+
+        if (data && data.length > 0) {
+            return data[0]; 
+        }
+    } catch (error) {
+        console.error('Erro ao buscar o post específico:', error);
+    }
 
     return {}
 }
